17c5cd046771c110ea93a475c1e7bf751c2ee055
AndrewSimons3 <simdrew07@gmail.com>
2021-06-30 20:06:34 -0500

    commit

--- swapi/src/App.js
+++ swapi/src/App.js
@@ -20,7 +20,7 @@ function App() {
         releaseDate: movieData.release_date 
       };
     });
-      setMovies(transformedMovies);
+      setMovies(transformedMovies);   
       setIsLoading(false);
   }
 
